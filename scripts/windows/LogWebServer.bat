b12cba55a3bdfe799e1ec800054db829d711cf26
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2023-08-21 01:15:03 -0700

    Updated Log scripts

---
+++ scripts/windows/LogWebServer.bat
@@ -0,0 +1,7 @@
+call _configs.bat
+title Remote Server Web Logs
+cd..
+
+set ARCHIVE=source.tar.bz2
+
+plink -ssh %USER%@%SERVER% -pw %PASSWORD% -m "./../../deployment/scripts/LogWebServer.sh" -batch
